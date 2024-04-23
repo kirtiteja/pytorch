887e4c6bc397d48335c442c52f645e72c26c90ad
Wang, Eikan <eikan.wang@intel.com>
2024-04-23 17:46:42 +0000

    Add cache lookup to accelerate torch.compile for eager
    ghstack-source-id: ff9b33285827bada4b1d989ecdbb30f565423928
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116368

---
+++ torch/csrc/inductor/aoti_eager/kernel_meta_info.h
@@ -0,0 +1,42 @@
+#if !defined(C10_MOBILE) && !defined(ANDROID)
+#pragma once
+
+#include <ATen/ATen.h>
+#include <c10/core/SymIntArrayRef.h>
+
+#include <string>
+
+namespace torch::inductor {
+
+class TensorChecker;
+
+struct TensorMetadata {
+  bool is_symbolic_;
+  c10::ScalarType dtype_;
+  c10::Device device_;
+  std::vector<c10::SymInt> sizes_;
+  std::vector<c10::SymInt> strides_;
+
+  TensorMetadata(const at::Tensor& src_tensor);
+  TensorMetadata(
+      bool is_symbolic,
+      c10::ScalarType dtype,
+      c10::Device device,
+      std::vector<c10::SymInt> sizes,
+      std::vector<c10::SymInt> strides);
+
+  bool operator==(const TensorMetadata& other) const;
+};
+
+struct TensorMetadataHash {
+  size_t operator()(const TensorMetadata&) const;
+};
+
+using AOTIKernelMetadata = std::vector<TensorMetadata>;
+
+struct AOTIKernelMetadataHash {
+  size_t operator()(const AOTIKernelMetadata&) const;
+};
+
+} // namespace torch::inductor
+#endif
